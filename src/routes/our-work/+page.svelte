7f7d75894be3e9212a3f36599ac9ee55bcf8a5c5
Lee Morgan <lee@leemorgan.dev>
2025-05-24 16:41:07 -0400

    Refactor the 'our work' page.

--- src/routes/our-work/+page.svelte
+++ src/routes/our-work/+page.svelte
@@ -1,90 +1,158 @@
 <script>
-    import blacklistAero from "$lib/clients/blacklist-aero.webp";
-    import logo from "$lib/favicon.png";
-    import utc from "$lib/utc.webp";
+    import blacklistImage from "$lib/clients/blacklist-aero.webp";
+    import inletSitesImage from "$lib/favicon.png";
+    import utcImage from "$lib/clients/utc.webp";
+
+    const items = [
+        {
+            title: "BLACKLIST.AERO",
+            text: "Platform for maintaining honesty and good practices in aviation",
+            src: blacklistImage,
+            url: "https://blacklist.aero"
+        },
+        {
+            title: "Inlet.Shop",
+            text: "E-commerce platform for local business to sell online",
+            src: inletSitesImage,
+            url: "https://inlet.shop"
+        },
+        {
+            title: "Unique Teak Creations",
+            text: "Teak furniture beautifylly handcrafted by skilled artisans",
+            src: utcImage,
+            url: "https://uniqueteakcreations.shop"
+        }
+    ];
 </script>
 
 <svelte:head>
     <title>Our Work | Inlet Sites</title>
+    <meta name="description" content="Portfolio pieces for Inlet Sites, a web development company">
 </svelte:head>
 
-<div class="container">
-    <h1>Our work</h1>
-    <h2>View some of the work done by Inlet Sites</h2>
-    <div class="divider"></div>
+<div class="OurWork">
+    <div class="header">
+        <h1>Our Work</h1>
+        <h2>Discover our work for other clients</h2>
+    </div>
 
-    <div class="sites">
-        <a href="https://blacklist.aero" class="card">
-            <p>BLACKLIST.AERO from Air Justice</p>
-            <img src={blacklistAero} alt="Blacklist.aero logo">
-        </a>
+    <div class="clients">
+        {#each items as item}
+            <a class="client" href={item.url} target="_blank">
+                <div class="imageContainer">
+                    <img src={item.src} alt={item.title}>
+                </div>
 
-        <a href="https://inlet.shop" class="card">
-            <p>Inlet.Shop from Inlet Sites</p>
-            <img src={logo} alt="Inlet Sites logo">
-        </a>
-
-        <a href="https://uniqueteakcreations.shop" class="card">
-            <p>Unique Teak Creations</p>
-            <img src={utc} alt="Unique Teak Creations">
-        </a>
+                <div class="right">
+                    <h2>{item.title}</h2>
+                    <p class="itemText">{item.text}</p>
+                    <p class="view">View Site</p>
+                </div>
+            </a>
+        {/each}
     </div>
 </div>
 
 <style>
-    .container{
+    .header{
         display: flex;
         flex-direction: column;
-        align-items: center;
-        margin: 75px auto;
-        width: 90%;
-    }
-
-    h1{
-        font-size: 38px;
-    }
-
-    h2{
-        text-align: center;
-    }
-
-    .divider{
-        border-bottom: 2px solid white;
-        width: 100%;
-    }
-
-    a{
-        display: flex;
-        flex-direction: row-reverse;
         justify-content: flex-end;
         align-items: center;
+        background: var(--darkBlue);
+        height: 250px;
+        padding-bottom: 35px;
         color: white;
-        margin: 35px 0;
-        font-size: 28px;
-        text-decoration: none;
-        box-shadow: 0 0 5px white;
-        border-radius: 15px;
-        height: 125px;
-        width: 100%;
-        max-width: 550px;
     }
 
-    a p{
-        font-size: 18px;
-        padding: 0 15px;
-        margin-left: 35px;
+    .header h1{
+        font-size: 55px;
+        margin-bottom: 25px;
+    }
+
+    .header h2{
         text-align: center;
     }
 
-    a img{
-        max-height: 100%;
-        border-top-left-radius: 15px;
-        border-bottom-left-radius: 15px;
-    }
-
-    .sites{
+    .clients{
         display: flex;
         flex-direction: column;
+        align-items: center;
+        width: 100%;
+        padding: 0 10px;
+    }
+
+    .client{
+        display: flex;
+        height: 250px;
+        width: 850px;
         max-width: 100%;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        margin: 35px 0;
+        text-decoration: none;
+        color: var(--darkBlue);
+    }
+
+    .imageContainer{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-basis: 300px;
+        flex-grow: 0;
+        background: var(--darkBlue);
+        height: 100%;
+        margin-right: 35px;
+    }
+
+    .imageContainer img{
+        max-height: 75%;
+        max-width: 75%;
+    }
+
+    .right{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        flex-basis: 100px;
+        flex-grow: 1;
+    }
+
+    .right h2{
+        font-size: 28px;
+    }
+
+    .itemText{
+        font-size: 18px;
+        color: var(--lightText);
+    }
+
+    .view{
+        font-size: 22px;
+        color: var(--lightBlue);
+        font-weight: bold;
+        text-decoration: underline;
+    }
+
+    @media screen and (max-width: 800px){
+        .client{
+            flex-direction: column;
+            min-height: 500px;
+        }
+
+        .imageContainer{
+            flex-basis: 200px;
+            width: 100%;
+            margin-right: 0;
+        }
+
+        .right{
+            padding: 15px;
+        }
+    }
+
+    @media screen and (max-width: 600px){
+        .header h1{
+            margin-bottom: 0;
+        }
     }
 </style>
